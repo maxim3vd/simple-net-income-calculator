794e5e095c0623a0674beb01c118d13a2ebe618c
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-24 17:32:36 +0100

    Define items as a dictionary with item names mapped to prices

---
+++ main.py
@@ -0,0 +1,9 @@
+# Dictionary mapping item names to their respective prices
+items = {
+    "Bubblegum": {"price": 2},
+    "Toffee": {"price": 0.2},
+    "Ice cream": {"price": 5},
+    "Milk chocolate": {"price": 4},
+    "Doughnut": {"price": 2.5},
+    "Pancake": {"price": 3.2}
+}
