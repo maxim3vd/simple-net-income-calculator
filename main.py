bdcd62be35e3299de9d04366de66ab58f7a4f80d
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-24 19:08:02 +0100

    Refactor items structure: Add quantity to each item

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# Dictionary mapping item names to their respective prices
+# Dictionary mapping item names to their respective prices and quantities
 items = {
-    "Bubblegum": {"price": 2},
-    "Toffee": {"price": 0.2},
-    "Ice cream": {"price": 5},
-    "Milk chocolate": {"price": 4},
-    "Doughnut": {"price": 2.5},
-    "Pancake": {"price": 3.2}
+    "Bubblegum": {"price": 2, "quantity": 101},
+    "Toffee": {"price": 0.2, "quantity": 590},
+    "Ice cream": {"price": 5, "quantity": 450},
+    "Milk chocolate": {"price": 4, "quantity": 420},
+    "Doughnut": {"price": 2.5, "quantity": 430},
+    "Pancake": {"price": 3.2, "quantity": 25}
 }
 
 # Display the header "Prices:"
